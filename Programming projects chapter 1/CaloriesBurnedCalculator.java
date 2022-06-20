147c68cdb2d51fc1e75d8dffabce361d1bd5f69f
Doruk Wagle <dorukwagle@gmail.com>
2022-06-20 21:33:17 +0545

    removed class files

---
+++ Programming projects chapter 1/CaloriesBurnedCalculator.java
@@ -0,0 +1,12 @@
+public class CaloriesBurnedCalculator{
+    public static void main(String[] args) {
+        // store calories burned in certain activities in MET
+        int caloriesRunning = 10;
+        int caloriesBasketball = 8;
+        int caloriesSleeping = 1;
+
+        float weightInKg = 150 / 2.2f;
+        
+
+    }
+}
