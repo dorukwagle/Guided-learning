97fa078e0562dc4270c4a4f710de398e94aa3089
Doruk Wagle <dorukwagle@gmail.com>
2022-06-22 20:25:14 +0545

    project 6 completed and .class files removed

--- week 1 self tests/Programming projects chapter 1/Project6.java
+++ week 1 self tests/Programming projects chapter 1/Project6.java
@@ -9,11 +9,26 @@ This would be considered a high cadence and a maximum gear size for a typical bi
 1/12 will result in 0, because both 1 and 12 are integers, and when the
 integer division is performed, the fractional part is discarded.
 */
+//1,1,1,1,
 
 public class Project6 {
     public static void main(String[] args) {
+        final float PI = 22 / 7;
+        //declare meter constant to convert meter to miles, 1 mile = 1609.34 meter
+        final float METERCONSTANT = 1609.34f;
         //gear size in meter
         float gearSize = 100 * 0.025f;
-        
+        //cadance in rpm
+        float cadance = 90;
+
+        //circumference of wheel
+        float circumference = gearSize * PI;
+
+        //calculate total distance covered in 1 hour in meter
+        float totalDistanceCovered = gearSize * cadance * 60; // 60 is for 1 hour
+
+        //finally convert meter to miles,
+        float mph = totalDistanceCovered / METERCONSTANT;
+        System.out.println("The speed of bycicle is: " + mph + " mph");
     }
 }
