0f19c755c700af8ac0d5d3cf378a22e3d8f70d01
Doruk Wagle <dorukwagle@gmail.com>
2022-06-22 18:54:19 +0545

    changed the folder to include all the week self test

--- Programming practice exercise 1/GreetingOfChoice.java
+++
@@ -1,5 +0,0 @@
-public class GreetingOfChoice{
-    public static void main(String[] arg){
-        System.out.println("Hello and welcome to pcps college.");
-    }
-}
--- Programming practice exercise 1/NameInsideTheBox.java
+++
@@ -1,7 +0,0 @@
-public class NameInsideTheBox {
-    public static void main(String[] args) {
-        System.out.println("_______");
-        System.out.println("|Doruk|");
-        System.out.println("-------");
-    }
-}
